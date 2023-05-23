80728c6cc879572d0d66074a28044304f42f157a
ShuaidongGG <gsd_2021@163.com>
2023-05-23 19:35:02 +0800

    Day05

--- main.go
+++ main.go
@@ -1,14 +1,23 @@
 package main
 
 import (
+	"Easy_IM/models"
 	"Easy_IM/router"
 	"Easy_IM/utils"
+	"time"
+
+	"github.com/spf13/viper"
 )
 
 func main() {
 	utils.InitConfig()
 	utils.InitMysql()
 	utils.InitRedis()
+	InitTimer()
 	r := router.Router()
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+}
+
+func InitTimer() {
+	utils.Timer(time.Duration(viper.GetInt("timeout.DelayHeartbeat"))*time.Second, time.Duration(viper.GetInt("timeout.HeartbeatHz"))*time.Second, models.CleanConnection, "")
 }
